3e9256003858323bc235fb48034d17a9d87bee49
mmbsow <mmbsow@gmail.com>
2014-08-30 10:15:35 -0400

    Programming assignment 8

--- mlclass-ex6-006/mlclass-ex6/dataset3Params.m
+++ mlclass-ex6-006/mlclass-ex6/dataset3Params.m
@@ -23,11 +23,24 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+C_vec = [0.01 0.03 0.1 0.3 1 3 10 30];
+sigma_vec = [0.01 0.03 0.1 0.3 1 3 10 30];
+errors = zeros(size(C_vec, 2), size(sigma_vec, 2));
 
+for i = 1:size(C_vec, 2)
+    for j = 1:size(sigma_vec, 2)
+        disp(C_vec(i));
+        disp(sigma_vec(j));
+        model = svmTrain(X, y, C_vec(i), @(x1, x2) gaussianKernel(x1, x2, sigma_vec(j)));
+        predictions = svmPredict(model, Xval);
+        errors(i,j) = mean(double(predictions ~= yval));
+    end
+end
 
-
-
-
+% find the coordinates of the lowest value of errors
+[row, col] = find(errors == min(errors(:)));
+C = C_vec(row);
+sigma = sigma_vec(col);
 
 % =========================================================================
 
--- mlclass-ex6-006/mlclass-ex6/processEmail.m
+++ mlclass-ex6-006/mlclass-ex6/processEmail.m
@@ -97,8 +97,10 @@ while ~isempty(email_contents)
     %       str2). It will return 1 only if the two strings are equivalent.
     %
 
-
-
+    index = find(strcmp(vocabList, str));
+    if not(isempty(index))
+        word_indices = [word_indices ; index];
+    end
 
 
 
